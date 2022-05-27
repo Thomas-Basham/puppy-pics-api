eadd2472089fb746cf3c83c949b8d2f515066c7d
Thomas Basham <Bashamtg@gmail.com>
2022-05-26 17:02:12 -0700

    fixed serializer

--- puppy_pics/serializers.py
+++ puppy_pics/serializers.py
@@ -4,5 +4,5 @@ from .models import PuppyPic
 
 class SnackSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ("id", "name", "image_url" "description", "added_by")
+        fields = ("id", "name", "img_url", "description", "added_by")
         model = PuppyPic
