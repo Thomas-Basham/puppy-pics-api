fa34e41a9a5e11a8304bbf813a593973608f59d4
Thomas Basham <Bashamtg@gmail.com>
2022-06-04 10:41:36 -0700

    moved form to index

--- pics/views.py
+++ pics/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView
 from puppy_pics.models import PuppyPic
-from puppy_pics.serializers import SnackSerializer
 from django.forms import ModelForm
+
+
 # Create your views here.
-
-
 class PuppyPicView(ListView):
     template_name = 'puppy_pic_list.html'
     model = PuppyPic
 
+    def get_context_data(self, **kwargs):
+        context = super(PuppyPicView, self).get_context_data(**kwargs)
+        context['form'] = PhotoForm()
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        context = dict(form=PhotoForm())
+
+        if request.method == 'POST':
+            form = PhotoForm(request.POST, request.FILES)
+            context['posted'] = form.instance
+            if form.is_valid():
+                form.save()
+                return redirect('puppy_pics_list')
+
+        return render(request, 'puppy_pic_list.html', context)
+
 
 class PhotoForm(ModelForm):
+
     class Meta:
         model = PuppyPic
         fields = '__all__'
@@ -20,7 +37,7 @@ class PhotoForm(ModelForm):
 
 def upload(request):
     context = dict(backend_form=PhotoForm())
-
+    context["dataset"] = PuppyPic.objects.all().order_by("-id")
     if request.method == 'POST':
         form = PhotoForm(request.POST, request.FILES)
         context['posted'] = form.instance
