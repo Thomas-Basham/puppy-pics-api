0cac776fd617fda3f27b0a6b9d58fc9c6b269551
Thomas Basham <Bashamtg@gmail.com>
2022-06-04 10:57:00 -0700

    styled form

--- pics/urls.py
+++ pics/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from .views import PuppyPicView, upload
+from .views import puppy_pic_view
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', PuppyPicView.as_view(), name='puppy_pics_list'),
-    path('upload/', upload, name='upload'),
+    path('', puppy_pic_view, name='upload'),
+    # path('upload/', upload, name='upload'),
     # path('<int:pk>', SnackDetailView.as_view(), name='snack_detail'),  # Integer, Primary Key
     # path('create/', PuppyPicCreateView.as_view(), name='puppy_pic_create'),
 
--- pics/views.py
+++ pics/views.py
@@ -5,37 +5,7 @@ from django.forms import ModelForm
 
 
 # Create your views here.
-class PuppyPicView(ListView):
-    template_name = 'puppy_pic_list.html'
-    model = PuppyPic
-
-    def get_context_data(self, **kwargs):
-        context = super(PuppyPicView, self).get_context_data(**kwargs)
-        context['form'] = PhotoForm()
-
-        return context
-
-    def post(self, request, *args, **kwargs):
-        context = dict(form=PhotoForm())
-
-        if request.method == 'POST':
-            form = PhotoForm(request.POST, request.FILES)
-            context['posted'] = form.instance
-            if form.is_valid():
-                form.save()
-                return redirect('puppy_pics_list')
-
-        return render(request, 'puppy_pic_list.html', context)
-
-
-class PhotoForm(ModelForm):
-
-    class Meta:
-        model = PuppyPic
-        fields = '__all__'
-
-
-def upload(request):
+def puppy_pic_view(request):
     context = dict(backend_form=PhotoForm())
     context["dataset"] = PuppyPic.objects.all().order_by("-id")
     if request.method == 'POST':
@@ -43,6 +13,46 @@ def upload(request):
         context['posted'] = form.instance
         if form.is_valid():
             form.save()
-        return redirect('puppy_pics_list')
+        return redirect('upload')
 
     return render(request, 'upload.html', context)
+
+
+# class PuppyPicView(ListView):
+#     template_name = 'puppy_pic_list.html'
+#     model = PuppyPic
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(PuppyPicView, self).get_context_data(**kwargs)
+#         context['form'] = PhotoForm()
+#
+#         return context
+#
+#     def post(self, request, *args, **kwargs):
+#         context = dict(form=PhotoForm())
+#
+#         if request.method == 'POST':
+#             form = PhotoForm(request.POST, request.FILES)
+#             context['posted'] = form.instance
+#             if form.is_valid():
+#                 form.save()
+#                 return redirect('puppy_pics_list')
+#
+#         return render(request, 'puppy_pic_list.html', context)
+class PhotoForm(ModelForm):
+    class Meta:
+        model = PuppyPic
+        fields = '__all__'
+
+
+# def upload(request):
+#     context = dict(backend_form=PhotoForm())
+#     context["dataset"] = PuppyPic.objects.all().order_by("-id")
+#     if request.method == 'POST':
+#         form = PhotoForm(request.POST, request.FILES)
+#         context['posted'] = form.instance
+#         if form.is_valid():
+#             form.save()
+#         return redirect('upload')
+#
+#     return render(request, 'upload.html', context)
